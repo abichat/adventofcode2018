f40adf75530acf421b38b45b3f4fdac6243736fc
abichat <a.bichat@yahoo.fr>
2018-12-05 17:54:59 +0100

    Day02

---
+++ Day02.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+
+#### Input ####
+
+words <- readLines("input_02.txt")
+
+
+#### Part One ####
+
+words %>%
+  tibble(word = .) %>% 
+  mutate(df_count = map(word, ~ tibble(letter = letters, count = str_count(., pattern = letters)))) %>% 
+  unnest(df_count) %>% 
+  filter(count %in% 2:3) %>% 
+  distinct(word, count) %>% 
+  group_by(count) %>% 
+  summarise(N = n()) %>% 
+  pull(N) %>% 
+  prod()
+
+
+#### Part Two ####
+
+N <- str_count(words[1])
+
+tibble(position = 1:N) %>% 
+  mutate(sub_words = map(position, ~ str_c(str_sub(words, 0, . - 1), str_sub(words, . + 1, N)))) %>% 
+  unnest(sub_words) %>% 
+  group_by(position, sub_words) %>%
+  summarise(N = n()) %>%
+  filter(N == 2) %>% 
+  pull(sub_words)
+  

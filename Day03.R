8a78c333392374de46c7b9e427ba5fa16f40ee7d
abichat <a.bichat@yahoo.fr>
2018-12-05 20:54:41 +0100

    Day03

---
+++ Day03.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+
+#### Input ####
+
+df_square <-
+  read_table("input_03.txt", col_names = FALSE, col_types = cols(X1 = col_character())) %>% 
+  separate(X1, into = c("ID", "Xinf", "Yinf", "Xlength", "Ylength"), sep = c(" @ |,|: |x")) %>% 
+  mutate(ID = str_sub(ID, start = 2)) %>% 
+  mutate_all(as.numeric) %>% 
+  mutate_at(vars(ends_with("inf")), ~ . + 1) %>% 
+  mutate(Xsup = Xinf + Xlength - 1, Ysup = Yinf + Ylength - 1)
+
+
+#### Part One ####
+
+Xmax <- max(df_square$Xsup)
+Ymax <- max(df_square$Ysup)
+
+M <- matrix(0, nrow = Ymax, ncol = Xmax)
+
+overlap <- function(Yinf, Ysup, Xinf, Xsup){
+  M[Yinf:Ysup, Xinf:Xsup] <<- M[Yinf:Ysup, Xinf:Xsup] + 1
+}
+
+df_square %>% 
+  select(-ID) %>% 
+  select(-ends_with("length")) %>% 
+  as.list() %>% 
+  pwalk(overlap)
+
+sum(M > 1)
+
+
+#### Part Two ####
+
+is_alone <- function(Yinf, Ysup, Xinf, Xsup){
+  all(M[Yinf:Ysup, Xinf:Xsup] == 1)
+}
+
+df_square %>% 
+  mutate(alone = pmap_lgl(list(Yinf, Ysup, Xinf, Xsup), is_alone)) %>% 
+  filter(alone) %>% 
+  pull(ID)

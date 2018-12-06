bc1392c12436da8059b223a60bac78701bf94e62
abichat <a.bichat@yahoo.fr>
2018-12-06 15:23:48 +0100

    Comments

--- Day01.R
+++ Day01.R
@@ -11,19 +11,19 @@ sum(vec)
 
 #### Part Two ####
 
-N <- length(vec)
-
-cs <- 0
-i <- 1
-s <- vec[1]
-
-while(! s %in% cs){
-  i <- i + 1
-  cs <- c(cs, s)
-  s <- s + vec[(i - 1) %% N + 1]
-}
-
-s
+# N <- length(vec)
+# 
+# cs <- 0
+# i <- 1
+# s <- vec[1]
+# 
+# while (!s %in% cs) {
+#   i <- i + 1
+#   cs <- c(cs, s)
+#   s <- s + vec[(i - 1) %% N + 1]
+# }
+# 
+# s
 
 ## Solution inspired from @mahendra-mariadassou
 
@@ -35,7 +35,7 @@ assign(as.character(0), value = NULL, envir = cs)
 i <- 1
 s <- vec[1]
 
-while (! exists(as.character(s), cs)) {
+while (!exists(as.character(s), cs)) {
   i <- i + 1
   assign(as.character(s), NULL, cs)
   s <- s + vec[(i - 1) %% N + 1]

45f9633832d04edf5ef38c23ea7b7679d00d76d2
abichat <a.bichat@yahoo.fr>
2018-12-12 10:59:02 +0100

    Cleaning

--- Day12.R
+++ Day12.R
@@ -2,57 +2,56 @@ library(tidyverse)
 
 #### Input ####
 
-rawinput <- readLines("input_12.txt")
+raw_input <- readLines("input_12.txt")
 
 initial_state <-
-  rawinput[1] %>%
+  raw_input[1] %>%
   str_remove("initial state: ")
 
 df_rules <-
-  rawinput[- (1:2)] %>% 
+  raw_input[- (1:2)] %>% 
   tibble(raw = .) %>% 
   separate(col = raw, into = c("seq", "result"), sep = " => ")
-
-G <- 20
 
 
 #### Part One ####
 
-S <- 0
+G <- 20
+
 first_pot <- 0
 current_state <- initial_state
 
-for(i in 1:G){
-  current_state <- 
-    current_state %>% 
+for(i in 1:G) {
+  current_state <-
+    current_state %>%
     str_c("....", ., "....")
   
-  df_current <- 
-    current_state %>% 
-    str_count() %>% 
-    {3:(.-3)} %>% 
-    tibble(pos = .) %>% 
+  df_current <-
+    current_state %>%
+    str_count() %>%
+    {3:(. - 3)} %>%
+    tibble(pos = .) %>%
     mutate(pot_number =  pos - 5 + first_pot,
-           seq = str_sub(current_state, start = pos - 2, end = pos + 2)) %>% 
+           seq = str_sub(current_state, start = pos - 2, end = pos + 2)) %>%
     left_join(df_rules, by = "seq")
   
   first_pot <-
-    df_current %>% 
-    filter(result == "#") %>% 
-    pull(pot_number) %>% 
+    df_current %>%
+    filter(result == "#") %>%
+    pull(pot_number) %>%
     first()
   
   current_state <-
-    df_current %>% 
-    pull(result) %>% 
-    str_c(collapse = "") %>% 
-    str_remove("^\\.*") %>% 
+    df_current %>%
+    pull(result) %>%
+    str_c(collapse = "") %>%
+    str_remove("^\\.*") %>%
     str_remove("\\.*$")
 }
 
-df_current %>% 
-  filter(result == "#") %>% 
-  pull(pot_number) %>% 
+df_current %>%
+  filter(result == "#") %>%
+  pull(pot_number) %>%
   sum()
 
 
@@ -60,47 +59,47 @@ df_current %>%
 
 G <- 50000000000
 
-S <- 0
 first_pot <- 0
 current_state <- initial_state
 
 seq_sum <- rep(NA, 1000)
 i <- 1
 
-while(i<G){
-  current_state <- 
-    current_state %>% 
+
+while (i < G) {
+  current_state <-
+    current_state %>%
     str_c("....", ., "....")
   
-  df_current <- 
-    current_state %>% 
-    str_count() %>% 
-    {3:(.-3)} %>% 
-    tibble(pos = .) %>% 
+  df_current <-
+    current_state %>%
+    str_count() %>%
+    {3:(. - 3)} %>%
+    tibble(pos = .) %>%
     mutate(pot_number =  pos - 5 + first_pot,
-           seq = str_sub(current_state, start = pos - 2, end = pos + 2)) %>% 
+           seq = str_sub(current_state, start = pos - 2, end = pos + 2)) %>%
     left_join(df_rules, by = "seq")
   
   current_pot_position <-
-    df_current %>% 
-    filter(result == "#") %>% 
+    df_current %>%
+    filter(result == "#") %>%
     pull(pot_number)
   
   first_pot <- current_pot_position[1]
   seq_sum[i] <- sum(current_pot_position)
   
-  if (i > 200){
-    if(all(diff(seq_sum[(i-100):i]) == seq_sum[i] - seq_sum[i-1])) {
-      increment <- seq_sum[i] - seq_sum[i-1]
+  if (i > 200) {
+    if (all(diff(seq_sum[(i - 100):i]) == seq_sum[i] - seq_sum[i - 1])) {
+      increment <- seq_sum[i] - seq_sum[i - 1]
       break
     }
   }
   
   current_state <-
-    df_current %>% 
-    pull(result) %>% 
-    str_c(collapse = "") %>% 
-    str_remove("^\\.*") %>% 
+    df_current %>%
+    pull(result) %>%
+    str_c(collapse = "") %>%
+    str_remove("^\\.*") %>%
     str_remove("\\.*$")
   
   i <- i + 1

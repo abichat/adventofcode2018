36297b7d2c9e990e64acf8403af5de2aba7fa0c3
abichat <a.bichat@yahoo.fr>
2018-12-12 10:22:17 +0100

    Sticker

--- hexasticker.R
+++ hexasticker.R
@@ -1,6 +1,6 @@
 library(hexSticker)
 
-sticker(subplot = "favicon.png", s_x = 1, s_y = 0.8,
+sticker(subplot = "favicon.png", s_x = 1, s_y = 0.8, s_width = 0.5,
         h_fill = "#0C1021", h_color = "#FFFB7C",
         package = "adventofcode2018", p_size = 4.5, p_color = "#FFFB7C",
         url = "adventofcode.com/2018", u_color = "#FFFB7C", u_size = 2,

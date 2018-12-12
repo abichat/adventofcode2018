3789bb37a64458e257e49fbfb01266fef9855059
abichat <a.bichat@yahoo.fr>
2018-12-12 10:53:00 +0100

    Day12II

--- Day12.R
+++ Day12.R
@@ -14,10 +14,9 @@ df_rules <-
   separate(col = raw, into = c("seq", "result"), sep = " => ")
 
 G <- 20
-# G <- 50000000000 # Will take > 100 days
 
 
-#### Part One and Two ####
+#### Part One ####
 
 S <- 0
 first_pot <- 0
@@ -38,13 +37,10 @@ for(i in 1:G){
     left_join(df_rules, by = "seq")
   
   first_pot <-
-    current_pot_position <-
     df_current %>% 
     filter(result == "#") %>% 
     pull(pot_number) %>% 
     first()
-  
-  first_pot <- current_pot_position[1]
   
   current_state <-
     df_current %>% 
@@ -58,3 +54,56 @@ df_current %>%
   filter(result == "#") %>% 
   pull(pot_number) %>% 
   sum()
+
+
+#### Part One and Two ####
+
+G <- 50000000000
+
+S <- 0
+first_pot <- 0
+current_state <- initial_state
+
+seq_sum <- rep(NA, 1000)
+i <- 1
+
+while(i<G){
+  current_state <- 
+    current_state %>% 
+    str_c("....", ., "....")
+  
+  df_current <- 
+    current_state %>% 
+    str_count() %>% 
+    {3:(.-3)} %>% 
+    tibble(pos = .) %>% 
+    mutate(pot_number =  pos - 5 + first_pot,
+           seq = str_sub(current_state, start = pos - 2, end = pos + 2)) %>% 
+    left_join(df_rules, by = "seq")
+  
+  current_pot_position <-
+    df_current %>% 
+    filter(result == "#") %>% 
+    pull(pot_number)
+  
+  first_pot <- current_pot_position[1]
+  seq_sum[i] <- sum(current_pot_position)
+  
+  if (i > 200){
+    if(all(diff(seq_sum[(i-100):i]) == seq_sum[i] - seq_sum[i-1])) {
+      increment <- seq_sum[i] - seq_sum[i-1]
+      break
+    }
+  }
+  
+  current_state <-
+    df_current %>% 
+    pull(result) %>% 
+    str_c(collapse = "") %>% 
+    str_remove("^\\.*") %>% 
+    str_remove("\\.*$")
+  
+  i <- i + 1
+}
+
+seq_sum[i] + 81 * (G - i)
